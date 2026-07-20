0f002181401f671a3799c3bdba8f58a816daa843
Israel-SoaresPorto <isaelporto@gmail.com>
2026-07-20 16:52:48 -0300

    refactor: corrigir definição de constantes de URL e base do diretório

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,4 @@
 <?php
 
-define('BASE_DIR', __DIR__);
-define('URL_BASE', 'http://' . $_SERVER['HTTP_HOST'] . "/sistema-eventos");
+define('BASE_DIR', dirname(__DIR__));
+define('URL_BASE', 'http://' . $_SERVER['HTTP_HOST'] . "/pessoal/sistema-eventos");
