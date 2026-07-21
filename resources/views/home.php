94f70389d6a27618014b5b260d095a6d25bdc55c
Israel-SoaresPorto <isaelporto15@gmail.com>
2026-07-21 10:02:34 -0300

    refactor: ajustar estrutura e layout da página inicial

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Eventos | Home</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
-    <h1><?php echo $message; ?></h1>
+    <?php include BASE_DIR . '/resources/components/evento/header.php'; ?>
+
+    <main class="container margin-top flex flex-center">
+        <div class="box-container text-center">
+            <h1><?php echo $message; ?></h1>
+            <p class="margin-top">Bem-vindo ao Sistema de Gerenciamento de Eventos.</p>
+        </div>
+    </main>
 </body>
 </html>
